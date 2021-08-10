a60760ddf90f85c9d2655508b728374311933dd5
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-08-10 13:20:11 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 
 clf = LyricPredictor()
 
+def clean_input(lyric):
+	lyric = lyric.lower()
+	clean_lyric = ""
+	for word in lyric.split(' '):
+		if word not in stop_words:
+			for char in bad_chars:
+				word = word.replace(char, "")
+			clean_lyric += word + ' '
+	clean_lyric = " ".join(clean_lyric.split())
+	return clean_lyric[:-1]
+
+
+
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -14,12 +27,13 @@ def index():
 
 @app.route('/predict', methods=["POST"])
 def predict():
-	lyric = str(request.form['lyric'])
+	og_lyric = str(request.form['lyric'])
+	lyric = clean_input(og_lyric)
 	lyric = lyric.capitalize()
 	lyric = '"' + lyric + '"'
 	artists = clf.predict(lyric)
 	artists = [i.title() for i in artists]
-	return render_template('index_pred.html', data = (artists, lyric))
+	return render_template('index_pred.html', data = (artists, og_lyric))
 
 
 if __name__ == '__main__':
